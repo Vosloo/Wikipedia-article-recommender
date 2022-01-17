bcf646e8b2396b019cc5ce576a1a527fa9859c2b
Wojciech Tomczak <gravemind@protonmail.com>
2022-01-17 18:56:21 +0100

    Add comment to web scraping method; Exclude pickle files

--- src/web_scraper.py
+++ src/web_scraper.py
@@ -7,8 +7,14 @@ from tqdm import tqdm
 import grequests
 
 if __name__ == '__main__':
+    """
+    Eyeballing it, with current setupt it should go to 4000 without problems.
+    After that wikipedia starts to thwart scraping attempts (some will pass, some won't).
+    If you need more articles consider using wikipedia dumps (wikipedia prefered 
+    method of downloading articles)
+    """
+
     responses = []
-    
     for _ in tqdm(range(cfg.NO_BATCHES), desc='Batches'):
         rqs = (grequests.get(cfg.RAND_WIKI) for _ in range(cfg.BATCH_SIZE))
         _responses = grequests.map(rqs)
